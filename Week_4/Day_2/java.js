72ed64fe8d57837abdd7a4e3e046cf6c168bde8a
ohmez <kkid_92@hotmail.com>
2018-09-06 21:34:54 -0600

    in class work week_4/day_2

---
+++ Week_4/Day_2/java.js
@@ -0,0 +1,32 @@
+// using for
+var name = '';
+for (var x = 0; x <100; x++) {
+
+}
+
+var arrNames = ["Itzae", "Keagan", ""];
+for ( var x = 0; x < arrNames.length; x++) {
+    console.log(arrNames[x]); 
+}
+
+var arrAnswers = ['rock', 'paper', 'scissors'];
+
+var guess = Math.floor(Math.random() * 3);
+console.log(arrAnswers[guess]);
+
+function yada (a,b,c) {
+    return a;
+}
+
+var yada2 = function (a,b,c) {
+
+};
+document.addEventListener("click", function (event) {
+    console.log('clicked');
+});
+
+
+var mainDiv = document.getElementById('test1');
+mainDiv.textContent = 'Ethan'
+mainDiv.innerHTML = 'Ethans'
+
