09b5235378d3210e169749b0cf4ff6dfb5233384
ohmez <41805219+ohmez@users.noreply.github.com>
2018-10-24 14:49:05 -0600

    Update read.js

--- Week_10_Node.js/read.js
+++ Week_10_Node.js/read.js
@@ -1,6 +1,11 @@
 var fs = require('fs');
+var file = "best_things_ever.txt";
+var format = "utf8";
+var newBest = process.argv[2];
 
-fs.readFile("best_things_ever.txt", "utf8", function(err, data) {
+function Fs () {
+  
+Fs.readFile = fs.readFile(file, format, function(err, data) {
   if (err) {
     return console.log(err);
   }
@@ -8,6 +13,12 @@ fs.readFile("best_things_ever.txt", "utf8", function(err, data) {
   
   for (var i = 0; i < data.length; i++) {
     console.log(data[i]);
+  } // end readfile
+Fs.append = fs.appendFile(file, newBest, function(err) {
+  if (err) {
+   return console.log(err);
   }
+  Fs.readFile();
+};
   
-  
+ 
