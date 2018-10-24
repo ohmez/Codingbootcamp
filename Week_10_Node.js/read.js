4dc61a6c02c08fb188d1f6f0b4a13cf65bedaa14
ohmez <41805219+ohmez@users.noreply.github.com>
2018-10-24 14:39:31 -0600

    Update read.js

--- Week_10_Node.js/read.js
+++ Week_10_Node.js/read.js
@@ -1,3 +1,13 @@
 var fs = require('fs');
 
-fs.readFile("best_things_ever.txt", "utf8");
+fs.readFile("best_things_ever.txt", "utf8", function(err, data) {
+  if (err) {
+    return console.log(err);
+  }
+   data = data.split(",");
+  
+  for (var i = 0; i < data.length; i++) {
+    console.log(data[i]);
+  }
+  
+  
