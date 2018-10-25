9d3a964824b983113d4505e98445de14b116cc82
ohmez <41805219+ohmez@users.noreply.github.com>
2018-10-24 19:23:12 -0600

    Create bank.js

---
+++ Week_10_Node.js/bank.js
@@ -0,0 +1,40 @@
+var fs = require('fs');
+var transation = process.argv[2];
+var amount = process.argv[3];
+function Fs (transaction, amount) {
+Fs.total = fs.readFile("bank.txt","utf8", function(err,data) {
+if (err) {
+reurn console.log(err);} else {
+var total = 0;
+data = data.split(',');
+for(x = 0; x < data.length; x++) {
+total += data[x];
+} console.log(total); // end for loop
+}); // end Fs.total 
+
+};// end Fs Function/object
+
+
+// rebuild switch only below, hopefullly call to functions using object/function calls. 
+switch(transaction) {
+case "total":
+fs.readFile("bank.txt", "utf8", function (err,data) {
+if(err) {
+return console.log(err);
+} else {
+data = data.spit(',');
+var total = 0;
+for(x = 0; x < data.length; x++) {
+total += data[x];
+} // end for loop
+}// end else statment
+});// end readfile function
+break;
+case "deposit": 
+fs.appendFile("bank.txt",amount, function (err) {
+if(err) {
+ return console.log(err);} else {
+ }
+});
+}// end switch function
+};
