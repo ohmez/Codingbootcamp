2320c8cf4e2abb4e5f339371e1e3bde4f53a1595
ohmez <kkid_92@hotmail.com>
2018-10-25 18:41:39 -0600

    updated saturday

--- Week_10_Node.js/bank.js
+++ Week_10_Node.js/bank.js
@@ -1,43 +1,48 @@
 var fs = require('fs');
 var transation = process.argv[2];
 var amount = process.argv[3];
-function Fs (transaction, amount) {
-Fs.total = fs.readFile("bank.txt","utf8", function(err,data) {
-if (err) {
-return console.log(err);} else {
-var total = 0;
-data = data.split(',');
-for(x = 0; x < data.length; x++) {
-total += data[x];
-} console.log(total); // end for loop
-} // end else
-});// end fs.total
+var bank = "bank.txt";
+var format = "utf8";
 
+switch(transation) {
+    case "withdraw":
+    withdraw();
+    break;
+    case "deposit":
+    deposit();
+    break;
+    case "total":
+    total();
+    break;
+    case "lotto":
+    lotto();
+    break;
 
+}
 
-};// end Fs Function/object
-
-
-// rebuild switch only below, hopefullly call to functions using object/function calls. 
-switch(transaction) {
-case "total":
-fs.readFile("bank.txt", "utf8", function (err,data) {
-if(err) {
-return console.log(err);
-} else {
-data = data.spit(',');
-var total = 0;
-for(x = 0; x < data.length; x++) {
-total += data[x];
-} // end for loop
-}// end else statment
-});// end readfile function
-break;
-case "deposit": 
-fs.appendFile("bank.txt",amount, function (err) {
-if(err) {
- return console.log(err);} else {
- }
-});
-}// end switch function
+function withdraw() {
+    fs.appendFile(bank, ", "+ (-amount), (err) => {
+        if(err) throw err;
+        console.log("Withdrew " + amount +" into account");
+    })
+};
+function deposit() {
+    fs.appendFile(bank, ", "+ amount, (err) => {
+        if(err) throw err;
+        console.log("deposited " + amount +" into account");
+    })
+};
+function total() {
+    var total = 0;
+    fs.readFile(bank,format, (err,data) => {
+        if(err) throw err;
+        data = data.split(',');
+        for (x=0; x < data.length; x++) {
+            // total = total + parseInt(data[x]);
+            total += parseFloat(data[x]);
+            parseInt(data[x]);
+            console.log(x + ": "+ parseFloat(data[x]));
+        }
+        console.log("your account balance is " + parseFloat(total).toFixed(2));
+    })
 };
--- Week_10_Node.js/read.js
+++ Week_10_Node.js/read.js
@@ -1,7 +1,7 @@
 var fs = require('fs');
 var file = process.argv[2];
 var format = "utf8";
-var newBest = "," + process.argv[3];
+var newBest = ", " + process.argv[3];
 
 // fs.readFile(file, function(err,data) {
 // if(err) {console.log(err);} else { var dataArr = data.split(","); console.log(dataArr);}
@@ -13,6 +13,16 @@ fs.readFile(file, format, (err,data) => {
 })
 }// end if no param given only read 
 if (process.argv[3]) {
+    if (process.argv[3] == 'split') {
+        fs.readFile(file,format,(err,data) => {
+            if(err) throw err; 
+            data = data.split(',');
+            for(x=0; x < data.length; x++) {
+                console.log(data[x]);
+            }// end for loop
+            });// end readFile function
+
+    } else {
 fs.appendFile(file, newBest, function(err) {
     if (err) {
     return console.log(err);
@@ -20,8 +30,9 @@ fs.appendFile(file, newBest, function(err) {
     fs.readFile(file, format,(err,data) => {
         if(err) throw err;
         console.log(data);
-    });
-  });
+    }); // end readfile after append. 
+  });// end append function
+}// end else
 }// end if adding content. 
 
 if(process.argv[4] == 'split') {
