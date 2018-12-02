e9fe60f9eaef23771b4d7fab3e40e7360275d3be
ohmez <kkid_92@hotmail.com>
2018-12-01 17:40:33 -0700

    saturday class notes

--- Week_15_sequelize/Saturday/BlogAuthor/routes/post-api-routes.js
+++ Week_15_sequelize/Saturday/BlogAuthor/routes/post-api-routes.js
@@ -5,7 +5,7 @@
 // Dependencies
 // =============================================================
 
-// Requiring our Todo model
+// Requiring our models
 var db = require("../models");
 
 // Routes
@@ -14,36 +14,57 @@ module.exports = function(app) {
 
   // GET route for getting all of the posts
   app.get("/api/posts", function(req, res) {
-    // Add sequelize code to find all posts, and return them to the user with res.json
-  });
-
-  // Get route for returning posts of a specific category
-  app.get("/api/posts/category/:category", function(req, res) {
-    // Add sequelize code to find all posts where the category is equal to req.params.category,
-    // return the result to the user with res.json
+    var query = {};
+    if (req.query.author_id) {
+      query.AuthorId = req.query.author_id;
+    }
+    db.Post.findAll({
+      where: query
+    }).then(function(dbPost) {
+      res.json(dbPost);
+    });
   });
 
   // Get route for retrieving a single post
   app.get("/api/posts/:id", function(req, res) {
-    // Add sequelize code to find a single post where the id is equal to req.params.id,
-    // return the result to the user with res.json
+    db.Post.findOne({
+      where: {
+        id: req.params.id
+      }
+    }).then(function(dbPost) {
+      console.log(dbPost);
+      res.json(dbPost);
+    });
   });
 
   // POST route for saving a new post
   app.post("/api/posts", function(req, res) {
-    // Add sequelize code for creating a post using req.body,
-    // then return the result using res.json
+    db.Post.create(req.body).then(function(dbPost) {
+      res.json(dbPost);
+    });
   });
 
   // DELETE route for deleting posts
   app.delete("/api/posts/:id", function(req, res) {
-    // Add sequelize code to delete a post where the id is equal to req.params.id, 
-    // then return the result to the user using res.json
+    db.Post.destroy({
+      where: {
+        id: req.params.id
+      }
+    }).then(function(dbPost) {
+      res.json(dbPost);
+    });
   });
 
   // PUT route for updating posts
   app.put("/api/posts", function(req, res) {
-    // Add code here to update a post using the values in req.body, where the id is equal to
-    // req.body.id and return the result to the user using res.json
+    db.Post.update(
+      req.body,
+      {
+        where: {
+          id: req.body.id
+        }
+      }).then(function(dbPost) {
+      res.json(dbPost);
+    });
   });
 };
---
+++ Week_15_sequelize/Saturday/BlogCRUD/routes/api-routes.js
@@ -0,0 +1,79 @@
+// *********************************************************************************
+// api-routes.js - this file offers a set of routes for displaying and saving data to the db
+// *********************************************************************************
+
+// Dependencies
+// =============================================================
+
+// Requiring our Todo model
+var db = require("../models");
+
+// Routes
+// =============================================================
+module.exports = function(app) {
+
+  // GET route for getting all of the posts
+  app.get("/api/posts", function(req, res) {
+    // Add sequelize code to find all posts, and return them to the user with res.json
+    db.Post.findAll({}).then((results) => {
+      return res.json(results);
+    })
+  });
+
+  // Get route for returning posts of a specific category
+  app.get("/api/posts/category/:category", function(req, res) {
+    // Add sequelize code to find all posts where the category is equal to req.params.category,
+    // return the result to the user with res.json
+    db.Post.findAll({where: {category: req.params.category}})
+    .then(results => res.json(results))
+  });
+
+  // Get route for retrieving a single post
+  app.get("/api/posts/:id", function(req, res) {
+    // Add sequelize code to find a single post where the id is equal to req.params.id,
+    // return the result to the user with res.json
+    db.Post.findAll({where: {id: req.params.id}})
+    .then(results => res.json(results))
+  });
+
+  // POST route for saving a new post
+  app.post("/api/posts", function(req, res) {
+    // Add sequelize code for creating a post using req.body,
+    // then return the result using res.json
+    db.Post.create({
+      title: req.body.title,
+      body: req.body.body,
+      category: req.body.category
+    }).then(results => res.json(results))
+  });
+
+  // DELETE route for deleting posts
+  app.delete("/api/posts/:id", function(req, res) {
+    // Add sequelize code to delete a post where the id is equal to req.params.id, 
+    // then return the result to the user using res.json
+    db.Post.destroy({
+      where: {
+        id: req.params.id
+      }
+    }).then(results => res.json(results))
+    // .then((results) => {
+    //   return res.json(results.dataValues)
+    // })
+      // return res.json(results) works as well, but only sending the key dataValues is a lot less information being sent, probably slightly safer
+    .catch((error) => {
+      return res.json(error);
+    })
+  });
+
+  // PUT route for updating posts
+  app.put("/api/posts", function(req, res) {
+    // Add code here to update a post using the values in req.body, where the id is equal to
+    // req.body.id and return the result to the user using res.json
+    db.Post.update({
+      title: req.body.title,
+      body: req.body.body,
+      category: req.body.category
+    }, {where: {id: req.body.id}})
+    .then(results => res.json(results))
+  });
+};
